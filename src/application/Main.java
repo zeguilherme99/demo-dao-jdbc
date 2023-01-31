8435b103012c7e7500c83d9fe8dec85f8a669a1e
zeguilherme99 <zeguilherme008@hotmail.com>
2023-01-31 15:47:38 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,5 +16,7 @@ public class Main {
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
         System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
     }
 }
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller Seller) {
+
+    }
+
+    @Override
+    public void update(Seller Seller) {
+
+    }
+
+    @Override
+    public void deleteById(Integer Id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer Id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
