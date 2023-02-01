468488c7c4f43877352875b709c162ba84be803e
zeguilherme99 <zeguilherme008@hotmail.com>
2023-01-31 21:27:08 -0300

    changing test numbers

--- src/application/MainDepartment.java
+++ src/application/MainDepartment.java
@@ -9,12 +9,12 @@ public class MainDepartment {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("\n==== TEST 4: seller insert =====");
+        System.out.println("\n==== TEST 1: seller insert =====");
         Department department = new Department(null, "Chocolate");
         departmentDao.insert(department);
         System.out.println("Created");
 
-        System.out.println("\n==== TEST 5: seller update =====");
+        System.out.println("\n==== TEST 2: seller update =====");
         department.setName("Chocolate Branco");
         departmentDao.update(department);
         System.out.println("Update completed");
