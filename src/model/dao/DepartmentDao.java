29693d054c79bea9f5baf4e9b1a7f1dbf43f8740
zeguilherme99 <zeguilherme008@hotmail.com>
2023-01-31 21:52:19 -0300

    changing parameters name toLowerCase

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -8,7 +8,7 @@ public interface DepartmentDao {
 
     void insert(Department department);
     void update(Department department);
-    void deleteById(Integer Id);
-    Department findById(Integer Id);
+    void deleteById(Integer id);
+    Department findById(Integer id);
     List<Department> findAll();
 }
