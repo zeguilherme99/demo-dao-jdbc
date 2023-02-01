98e7eba859f6a54a925dae309795bf82237b385c
zeguilherme99 <zeguilherme008@hotmail.com>
2023-01-31 21:46:24 -0300

    fix sout message to department

--- src/application/MainDepartment.java
+++ src/application/MainDepartment.java
@@ -11,21 +11,21 @@ public class MainDepartment {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("\n==== TEST 1: seller insert =====");
+        System.out.println("\n==== TEST 1: department insert =====");
         Department department = new Department(null, "Chocolate");
         departmentDao.insert(department);
         System.out.println("Created");
 
-        System.out.println("\n==== TEST 2: seller update =====");
+        System.out.println("\n==== TEST 2: department update =====");
         department.setName("Chocolate Branco");
         departmentDao.update(department);
         System.out.println("Update completed");
 
-        System.out.println("\n==== TEST 3: seller findAll =====");
+        System.out.println("\n==== TEST 3: department findAll =====");
         List<Department> departments = departmentDao.findAll();
         departments.forEach(System.out::println);
 
-        System.out.println("\n==== TEST 4: seller delete =====");
+        System.out.println("\n==== TEST 4: department delete =====");
         departmentDao.deleteById(8);
         System.out.println("Deleted");
     }
